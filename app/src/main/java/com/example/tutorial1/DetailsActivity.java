03ad39736433d81fbabcbb88ba97c95addd23cc4
auviera <auviera12@gmail.com>
2020-12-16 07:13:44 +0700

    Tutorial 3 & 4

--- app/src/main/java/com/example/tutorial1/DetailsActivity.java
+++ app/src/main/java/com/example/tutorial1/DetailsActivity.java
@@ -1,21 +1,57 @@
 package com.example.tutorial1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class DetailsActivity extends AppCompatActivity {
 
+    private String url;
+    private String name;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
-        String name = getIntent().getStringExtra("name");
-        String url = "https://en.m.wikipedia.org/wiki/Android_"+ name.replace(" ", "_");
+        name = getIntent().getStringExtra("name");
+
+        url = "https://en.m.wikipedia.org/wiki/Android_"+ name.replace(" ", "_");
         WebView webView = findViewById(R.id.webView);
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl(url);
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_details,menu);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+
+        switch(item.getItemId()) {
+            case R.id.action_share:
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_TEXT, "Hi there, my name is Auviera Pascafiwi (1201502578) and i'm sending the details of "+name+" here "+url);
+                startActivity(intent);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
+
+
+
 }
